bd272004065541d4a4115556793530c1471f511a
Ana Bea <116468669+beana15@users.noreply.github.com>
2022-11-05 23:27:57 +0000

    Adicionei um módulo chamado ExcecaoUsuario. Este módulo irá tratar as exceções da classe Usuario.

---
+++ ExcecaoUsuario.py
@@ -0,0 +1,14 @@
+class ExcecaoNome(Exception, ValueError, TypeError):
+  pass
+
+class ExcecaoEmail(Exception):
+  pass
+
+class ExcecaoSenha(Exception):
+  pass
+#KeyError
+#NameError
+class ExcecaoLogin(Exception):
+  pass
+#KeyError
+#NameError
