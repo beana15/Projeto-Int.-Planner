da290e1a134b202c65d95c5a392bc51ccd162321
Mary Helen <93613631+moaninha7@users.noreply.github.com>
2022-11-08 23:21:55 +0000

    adicionei labels, buttons e um def

--- Interface.py
+++ Interface.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import os
 cor1 = '#ff8826'
 cor2 = '#fed35b'
 cor3 = '#ffd78f'
@@ -10,5 +10,13 @@ cor7 = '#2c9fa3'
 
 principal = Tk()
 principal.title('PlanLion')
-principal.configure(bg = cor1)
+principal.configure(bg = cor3)
 principal.geometry('400x400+200+200')
+label1=Label(text='Seja Bem-vinda(o)\n\nO PlanLion é uma ferramenta que planejamos para sua própria organização',bg=cor3)
+label1.place(x='100',y='100')
+
+def bt_click():
+  os.system("clear")
+
+botao1=Button(principal,command=bt_click,text='Continuar',background=cor1)
+botao1.place(x='285',y='200')
