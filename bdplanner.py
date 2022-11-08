d3e4c20582175c4e0da230dd55b3946fc7dd3309
beana15 <116468669+beana15@users.noreply.github.com>
2022-11-08 14:58:38 -0400

    Planner projeto 4b (#2)
    * Adicionei um módulo chamado ExcecaoUsuario. Este módulo irá tratar as exceções da classe Usuario.
    
    * atualização do banco de dados
    
    Co-authored-by: Geovanna Guimarães <115263581+Geovanaal@users.noreply.github.com>

---
+++ bdplanner.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+arquivo = "banco.bd"
+
+def iniciar_bdplanner():
+  bdplanner = None
+  try:
+    bdplanner = sqlite3.connect(arquivo.banco)
+    
+  except sqlite3.error as e:
+    print("Erro na conexão",e)
+    
+  return bdplanner
+
+def criar_tabela(bdplanner,sql_criar_tabela):
+  try:
+    cursor = bdplanner.cursor()
+    cursor.execute(sql_criar_tabela)
+  except sqlite3.Error as e:
+    print("Erro ao criar a tabela",e)
+    
+
+    def inserir_usuario(bdplanner, sql_inserir_usuario):
+      try:
+        cursor = bdplanner.cursor()
+        cursor.execute(sql_inserir_usuario)
+        bdplanner.commit()
+        print('Conexão estabelecida')
+      except sqlite3.Error as e:
+        print('Erro na inserção de dados:', e)
+
+def buscar_usuario(bdplanner, sql_buscar_usuario):
+  usuario = None
+  try:
+    cursor = bdplanner.cursor()
+    cursor.execute(sql_buscar_usuario)
+    usuario = cursor.fetchall()
+    print('exibição de dados concedida.')
+  except sqlite3.Error as e:
+    print('Erro exibindo os dados:', e)
+  finally:
+    return usuario
+    
