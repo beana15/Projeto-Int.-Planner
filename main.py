9e23bf9dcd8bdbd9102ca8f16a7aedb76956d566
Ana Bea <116468669+beana15@users.noreply.github.com>
2022-11-08 18:00:37 +0000

    Adicionei o módulo interface, importei o tkinter e criei a tela.

--- main.py
+++ main.py
@@ -3,60 +3,72 @@
 #Mary Helen
 #Ana Beatriz de Oliveira
 
-from Usuario import *
-from Calendario import *
-from Planner import *
-from Compromisso import *
 
-import os
+from tkinter import *
+from Interface import *
 
-inicio = input('Seja bem-vindo ao Planner Leão!!! \n\nPara começar sua experiência você primeiro precisará se cadastrar ou fazer seu login. \n\nDeseja continuar? ')
-os.system("clear")
+principal.mainloop()
 
-if inicio == 'sim' or inicio == 'Sim' or inicio == 'SIM':
-  cl = input('1 - Cadastrar \n2 - Logar \n\n')
+
+
+
+
+
+
+#from Usuario import *
+#from Calendario import *
+#from Planner import *
+#from Compromisso import *
+
+#import os
+
+#inicio = input('Seja bem-vindo ao Planner Leão!!! \n\nPara começar sua experiência você primeiro precisará se cadastrar ou fazer seu login. \n\nDeseja continuar? ')
+#os.system("clear")
+
+#if inicio == 'sim' or inicio == 'Sim' or inicio == 'SIM':
+  #cl = input('1 - Cadastrar \n2 - Logar \n\n')
 #while True:
-  if cl != 2:
-   print('CADASTRO \n')
-   usuario = Usuario(' ', ' ', ' ', ' ')
-   os.system("clear")
-   while True:
-    opcao = input('Seu cadastro foi realizado com sucesso! \n\nDeseja realizar mais algum cadastro no seu Planner Leão? ')
-    os.system('clear')
-    if (opcao == 'não' or opcao == 'Não' or opcao == 'NÃO'):
-     break
+  #if cl != 2:
+   #print('CADASTRO \n')
+   #usuario = Usuario(' ', ' ', ' ', ' ')
+   #os.system("clear")
+   #while True:
+    #opcao = input('Seu cadastro foi realizado com sucesso! \n\nDeseja realizar mais algum cadastro no seu Planner Leão? ')
+    #os.system('clear')
+    #if (opcao == 'não' or opcao == 'Não' or opcao == 'NÃO'):
+   #  break
 
-  else:
-   login = input('Login: ')
-   senha = input('Senha: ')
+  #else:
+   #login = input('Login: ')
+   #senha = input('Senha: ')
    #dicionario
-   login = {self.login: self.__senha}
-   try:
-    os.system('clear')
-    selecione = int(input('O que deseja fazer? \n\n1 - Marcar evento \n2 - Consultar seus compromissos \n3 - Consultar compartilhamento do Planner \n4 - Ir para o calendário \n\nSelecione uma opção: '))
-    planner = Planner(' ', ' ', ' ', ' ') 
+   #login = {self.login: self.__senha}
+   #try:
+    #os.system('clear')
+    #selecione = int(input('O que deseja fazer? \n\n1 - Marcar evento \n2 - Consultar seus compromissos \n3 - Consultar compartilhamento do Planner \n4 - Ir para o calendário \n\nSelecione uma opção: '))
+    #planner = Planner(' ', ' ', ' ', ' ') 
      
-    if planner == 1:
-     comp = Compromisso(' ',' ',' ',' ',' ')
+    #if planner == 1:
+     #comp = Compromisso(' ',' ',' ',' ',' ')
       
-    elif planner == 2:
-     comp1 = Compromisso(' ',' ',' ',' ',' ')
-     print(comp1.getconsultarCompromisso(' ', ' ', ' ', ' ', ' '))
-     setmodificarAgenda()
+    #elif planner == 2:
+     #comp1 = Compromisso(' ',' ',' ',' ',' ')
+     #print(comp1.getconsultarCompromisso(' ', ' ', ' ', ' ', ' '))
+     #setmodificarAgenda()
 
-    elif planner == 3:
-     usuario = Usuario(' ', ' ', ' ', ' ')
-     print(usuario.getconsultarUsuario()
+    #elif planner == 3:
+     #usuario = Usuario(' ', ' ', ' ', ' ')
+    # print(usuario.getconsultarUsuario()
 
-    else:
-     getconsultarCalendario()
-     marcarCompromisso()
+   # else:
+     #getconsultarCalendario()
+    # marcarCompromisso()
       
-   except:
-     print('Erro na senha ou login. Por favor tente novamente.')
+   #except:
+  #   print('Erro na senha ou login. Por favor tente novamente.')
 
-else:
-  print('Bye, bye')
+#else:
+ # print('Bye, bye')
    
 
 #usuario = Usuario(' ', ' ', ' ', ' ')
